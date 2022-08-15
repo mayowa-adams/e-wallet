8c30b1544f368eb3a5fc6da8182b93a4af985459
mayowa-adams <adamsmayowa06@gmail.com>
2022-08-15 02:16:19 +0100

    hi

---
+++ src/Pages/LoginOptions.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './LoginOptions.css'
+import splax from '../components/assets/OaletLogo.svg'
+// console.log(splax)
+function LoginOptions() {
+  return (
+    <div className='view'>
+        <img src={splax} alt=''/>
+    </div>
+  )
+}
+
+export default LoginOptions
---
+++ src/Pages/Send.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Send.css'
+
+function Send() {
+  return (
+    <div className="abs">Send</div>
+  )
+}
+
+export default Send
